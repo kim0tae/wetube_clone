4f5bdf5330a714a782d024021afd6f44739ad488
Kim younftae <kimyt0402@gmail.com>
2023-12-28 13:44:34 +0900

    [UPDATE] 화면 렌더링 초기 개발

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,7 @@
 export const edit = (req, res) => res.send("Edit Users")
 export const remove = (req, res) => res.send("Remove Users")
 export const join = (req, res) => res.send("join")
+export const login = (req, res) => res.send("login")
+export const see = (req, res) => res.send("see")
+export const logout = (req, res) => res.send("logout")
+
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,16 @@
 
-export const watch = (req, res) => res.send("Watch Video")
-export const edit = (req, res) => res.send("Edit Video")
-export const trending = (req, res) => res.send("Home Page Videos")
+export const see = (req, res) => {
+  return res.render("watch")
+}
+export const edit = (req, res) => {
+  res.render("edit")
+}
+export const trending = (req, res) => {
+  res.render("home")
+}
+export const search = (req, res) => res.send("search")
+export const deleteVideo = (req, res) => {
+  return res.send("deleteVideo")
+}
+export const upload = (req, res) => res.send("upload")
 
