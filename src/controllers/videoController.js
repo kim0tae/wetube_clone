84ce2261d35dc2732e4856f6b7363e24f7229040
Kim younftae <kimyt0402@gmail.com>
2023-12-30 17:39:28 +0900

    [UPDATE] Edit Video 2차 수정
    6.22

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,5 @@
 import Video from "../models/Video";
-
+// ======================================
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
@@ -8,12 +8,29 @@ export const watch = async (req, res) => {
   }
   return res.render("404", { pageTitle: "Not Found Video" });
 };
-
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  if (videos) {
+    return res.render("home", { pageTitle: "Home", videos })
+  }
+}
+// ======================================
 export const postEdit = async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
   const video = await Video.findById(id);
-
-  return res.redirect(`/videos/${id}`, video);
+  const { title, description, hashtags } = req.body;
+  
+  if (!video) {
+    return res.render("404", { pageTitle: "Not Found Video" });
+  }
+  
+  video.title = title;
+  video.description = description;
+  video.hashtags = hashtags
+    .split(',')
+    .map((word) => (word.startsWith('#') ? word : `#${word}`));
+  await video.save(); 
+  return res.redirect(`/videos/${id}`);
 };
 export const getEdit = async (req, res) => {
   const { id } = req.params;
@@ -23,17 +40,10 @@ export const getEdit = async (req, res) => {
   }
   return res.render("edit", { pageTitle :  `Edit : ${video.title}`, video});
 }
-export const home = async (req, res) => {
-    const videos = await Video.find({});
-    if (videos) {
-      return res.render("home", { pageTitle: "Home", videos })
-    }
-}
+// ======================================
 export const deleteVideo = (req, res) => {
   return res.send("deleteVideo")
 }
-export const upload = (req, res) => res.send("upload")
-
 
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle : "Upload Video"});
@@ -46,7 +56,9 @@ export const postUpload = async (req, res) => {
       title: title,
       description : description,
       createdAt: Date.now(),
-      hashtags: hashtags.split(',').map(word => `#${word}`),
+      hashtags: hashtags
+        .split(",")
+        .map((word) => (word.startsWith("#") ? word : `#${word}`)),      
     })
     return res.redirect("/");
   } catch (error) {
@@ -57,3 +69,4 @@ export const postUpload = async (req, res) => {
       })
   }
 }
+// ======================================
