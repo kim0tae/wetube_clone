7e15ae0d155784b59fc857f910c3ca79c474ac5b
Kim younftae <kimyt0402@gmail.com>
2024-01-03 14:57:09 +0900

    [UPDATE] [UPDATE] 깃허브 로그인 추가 - 3차

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 // ========================================
 export const getJoin = (req, res) => {
@@ -59,9 +60,8 @@ export const see = (req, res) => res.send("see");
 export const logout = (req, res) => res.send("logout");
 export const startGithubLogin = (req, res) => { 
   const baseUrl = `https://github.com/login/oauth/authorize`
-  const client_id = process.env.CLIENT_ID;
   const config = {
-    client_id,
+    client_id: process.env.GH_CLIENT,
     allow_signup: false,
     scope: "read:user user:email",
   }
@@ -71,7 +71,37 @@ export const startGithubLogin = (req, res) => {
   return res.redirect(finalUrl);
 }
 
-export const finishGithubLogin = (req, res) => {
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  ).json();
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const userRequest = await (
+      await fetch("https://api.github.com/user", {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(userRequest);
+  } else {
+    return res.redirect("/login");
+  }
   
+//  return res.render(finalUrl);
 }
 
