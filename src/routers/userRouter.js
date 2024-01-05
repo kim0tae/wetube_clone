162d96146a5a7f79ccd50c11e4a40ce93ba0d9f0
Kim younftae <kimyt0402@gmail.com>
2024-01-05 14:05:00 +0900

    [FEATURE] 비디오 업로드 추가

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,13 +1,13 @@
 import express from "express"
 import { getEdit, postEdit, remove, see, logout, startGithubLogin, finishGithubLogin, getChangePassword, postChangePassword} from '../controllers/userController'
-import { protectorMiddleware, publicOnlyMiddleware, uploadFiles} from "../middlewares";
+import { protectorMiddleware, publicOnlyMiddleware, avatarUpload} from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware,logout);
 userRouter.route("/edit").all(protectorMiddleware)
   .get(getEdit)
-  .post(uploadFiles.single("avatar"), postEdit);
+  .post(avatarUpload.single("avatar"), postEdit);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/remove", remove);
