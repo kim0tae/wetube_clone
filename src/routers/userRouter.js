dca5f0fa3d176bbe7a819f15e3d1162bc2042005
Kim younftae <kimyt0402@gmail.com>
2024-01-05 11:14:17 +0900

    [UPDATE] procted Middleware 적용
    로그인 유무에 따라 접근이 가능한 URL이 있거나 안되는 URL이 있어서 처리.

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,14 +1,15 @@
 import express from "express"
 import { getEdit, postEdit, remove, see, logout, startGithubLogin, finishGithubLogin } from '../controllers/userController'
+import { proectedMiddleware, publicOnlyMiddleware} from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.route("/edit").get(getEdit).post(postEdit);
+userRouter.get("/logout", proectedMiddleware,logout);
+userRouter.route("/edit").all(proectedMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/remove", remove);
 userRouter.get("/:id", see);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
 
 
 export default userRouter;
